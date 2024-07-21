4a75f031ea25fd6aa8ffdafb3ce03eb5f9ad90e5
lizhijun <lizhijunx@gmail.com>
2024-07-21 10:28:31 +0800

    feat:add footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
+import { SiImagedotsc, SiApachespark, SiTwitter, SiSafari } from 'react-icons/si'
 import { Button } from './ui/button'
+// https://react-icons.github.io/react-icons/icons/si/
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
-         </footer>
+   <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
+      <div className="flex justify-center">
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className="itext-muted-foreground/50"
+        >
+          <Link href="https://toolify.jiehuo.ai/" target="_blank">
+            <SiImagedotsc size={18} />
+          </Link>
+        </Button>
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className="1text-muted-foreground/50"
+        >
+          <Link href="https://toolify.jiehuo.ai/" target="_blank">
+            <SiApachespark size={18} />
+          </Link>
+        </Button>
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className="1text-muted-foreground/50"
+        >
+          <Link href="https://www.jiehuo.ai/baike" target="_blank">
+            <SiSafari size={18} />
+          </Link>
+        </Button>
+      </div>
+    </footer> 
   )
 }
 
